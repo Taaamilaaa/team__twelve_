74b1aa366d84fbd770d226b1568ab8176fd1c39b
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-09-16 12:09:42 +0300

    added all files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 import './sass/main.scss';
+import './js/api-service';
+import ApiService from './js/api-service';
+import cardTemplate from './templates/card-image.hbs';
+
+const refs = {
+
+  searchForm: document.querySelector('#js-input-country'),
+  container: document.querySelector('.container-list')
+
+
+};
+
+const apiService = new ApiService();
+
+apiService.fetchApi().then(res => {
+  console.log(...res.data._embedded.events);
+  renderEvents(res.data._embedded.events);
+});
+
+function renderEvents(event) {
+  refs.container.insertAdjacentHTML('beforeend', cardTemplate(event));
+}
