5abd39f953eada73f3a11880f646657572ef06e8
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-09-16 15:01:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './sass/main.scss';
 import './js/api-service';
 import ApiService from './js/api-service';
 import cardTemplate from './templates/card-image.hbs';
+<<<<<<< Updated upstream
+=======
+import { locale, page, searchQuery } from './js/eventSearch';
+>>>>>>> Stashed changes
 
 
 const refs = {
