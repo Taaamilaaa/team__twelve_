83b9c3b36cdc86f0ef8d2593be970ecaad0344d4
RybalovValentyn <0661629069@ukr.net>
2021-09-16 12:53:59 +0300

    event search

---
+++ index.js
@@ -0,0 +1,23 @@
+import './sass/main.scss';
+import './js/api-service';
+import ApiService from './js/api-service';
+import cardTemplate from './templates/card-image.hbs';
+import {locale, page, searchQuery} from './js/loading-event'
+const refs = {
+
+    searchForm: document.querySelector('#js-input-country'),
+    container: document.querySelector('.container-list')
+
+ 
+};
+
+const apiService = new ApiService(locale, page, searchQuery);
+
+apiService.fetchApi().then(res => {
+  console.log(...res.data._embedded.events);
+  renderEvents(res.data._embedded.events);
+});
+
+function renderEvents(event) {
+  refs.container.insertAdjacentHTML('beforeend', cardTemplate(event));
+}
