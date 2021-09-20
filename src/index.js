a98f0135d64f74c562c2f6d54f95d64ba052d33c
Rom0105 <savosko.photo@gmail.com>
2021-09-20 12:39:02 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import './js/api-service';
 import './js/start-loading';
 import './js/countrySearch';
 import './js/scroll-top-btn';
-import './js/dropdown-menu';
 import './js/team';
 import './js/modal-open';
